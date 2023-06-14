384e7462e8e6982cc0aa2cf024abd1c91f51fea6
Soe Phyu <soephyuphyuhtun99@gmail.com>
2023-06-14 09:23:45 +0630

    Exercise - Stop Watch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+// Exercise - Stop Watch
+let interval;
+function StopWatch() {
+    this.duration = 0;
+    this.isSet = false;
+    this.start = () => {
+        if (this.isSet) {
+            throw new Error('It is already starting!')
+        }
+        interval = setInterval(() => this.duration++, 1000);
+        this.isSet = true;
+        console.log('starting counting...');
+    };
+    this.stop = () => {
+        if (!this.isSet) {
+            throw new Error('It is not starting yet!')
+        }
+        clearInterval(interval)
+        console.log('duration : ', this.duration)
+        this.isSet = false;
+    };
+    this.reset = () => {
+        this.duration = 0;
+    }
+}
+
+let watch = new StopWatch();
